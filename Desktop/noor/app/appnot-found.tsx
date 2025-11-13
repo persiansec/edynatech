c18aabcf2684e81232ad6af82634d4995918edec
persiansec <98105491+persiansec@users.noreply.github.com>
2025-11-14 00:26:00 +0330

    Delete Desktop/noor directory

--- Desktop/noor/app/appnot-found.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-  return (
-    <div className="text-center p-10">
-      <h1 className="text-4xl font-bold">صفحه مورد نظر پیدا نشد</h1>
-      <p className="mt-4 text-gray-600">لطفاً آدرس را بررسی کنید یا به صفحه اصلی برگردید.</p>
-    </div>
-  );
-}
